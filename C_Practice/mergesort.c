b121cecc2e183198622dab282970464294ff5210
Vikram Manja <vmanja@purdue.edu>
2018-07-16 16:22:08 -0700

    Added mergesort of linked lists in C

--- C_Practice/mergesort.c
+++ C_Practice/mergesort.c
@@ -1,6 +1,17 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
+struct node* mergeList(struct node* a, struct node* b);
+void splitList(struct node* source, struct node** frontRef, struct node** backRef);
+void printList(struct node *node)
+
+
+struct node
+{
+	int data;
+	struct node* next;
+};
 
 void merge(int * arr, int low, int high){
 	int mid = (high+low)/2;
@@ -63,6 +74,114 @@ void msort(int * arr, int low, int high){
 void mergesort(int * arr, int len){
 	msort(arr, 0, len-1);
 } 
+
+
+
+{
+
+
+
+void printList(struct node *node)
+{
+    while(node!=NULL){
+        printf("%d ", node->data);
+        node = node->next;
+    }
+    printf("\n");
+}
+ 
+int listLength(struct node * head){
+    int count = 0;
+    while(head != NULL){
+        count++;
+        head = head->next;
+    }
+    return count;
+}
+
+/* split the nodes of the given list into front and back halves,
+     and return the two lists using the reference parameters.*/
+void splitList(struct node* source, struct node** frontRef, struct node** backRef)
+{
+    // set front and back to head
+    // Guaranteed length is 2+
+    
+    /* Debug
+    printf("Original : "); 
+    printList(source);
+    */
+    
+    *frontRef = source; 
+    struct node * ptr = source;
+    int length = listLength(source);
+    // move ptr to half way point
+    for (int i = 0; i < length/2-1; i++){
+        ptr = ptr->next;
+    }
+    // split the list
+    *backRef = ptr->next;
+    ptr->next = NULL;
+    
+    /* Debug
+    printf("Front : "); 
+    printList(*frontRef); 
+    printf("Back : "); 
+    printList(*backRef);
+    */
+    
+}
+// Returns 1 if a->data < b->data else 0
+int compareNodes(struct node * a, struct node * b){
+    if (a == NULL) return 0;
+    if (b == NULL) return 1;
+    return (a->data < b->data);    
+}
+// merges two sorted list into one big list
+struct node* mergeList(struct node* a, struct node* b)
+{
+    /* Debug
+    printf("Merging\n");
+    printf("A: ");printList(a);printf("B: ");printList(b);
+    */
+    // Initialize Head, a, b
+    struct node * head = (compareNodes(a,b)) ? a : b;
+    if (compareNodes(a,b)) {
+        a = a->next;
+    }else{
+        b = b->next;
+    }
+    struct node * tail = head;
+    // Compare and Merge
+    while((a != NULL) || (b != NULL)){
+        if (compareNodes(a,b)){
+            tail->next = a;
+            a = a->next;
+        }else{
+            tail->next = b;
+            b = b->next;
+        }
+        tail = tail->next;
+    }
+    /* Debug
+    printf("Head now: ");
+    printList(head);
+    */
+    return head;
+    
+}
+
+void mergeSortList(struct node** headRef)
+{
+    struct node* head = *headRef;
+    struct node* a;
+    struct node* b;
+    // Lenght < 2 is already sorted 
+    if ((head == NULL) || (head->next == NULL)) return;
+    splitList(head, &a, &b); 
+    mergeSortList(&a);
+    mergeSortList(&b);
+    *headRef = mergeList(a, b);
+}
 
 
 int main(void){
